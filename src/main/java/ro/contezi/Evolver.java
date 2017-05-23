fc473119296adc03c01af25c7efc23babfa177c1
catalin <lazcatluc@gmail.com>
2017-05-23 19:55:00 +0300

    Add survival when 2 neighbors

--- src/main/java/ro/contezi/Evolver.java
+++ src/main/java/ro/contezi/Evolver.java
@@ -1,7 +1,17 @@
 package ro.contezi;
 
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
 public class Evolver {
+
 	public Universe evolve(Universe original) {
-		return new Universe();
+		Map<Integer, Set<Cell>> cellsByNeighbors = new HashMap<>();
+		original.getCells().forEach(cell -> cellsByNeighbors
+				.computeIfAbsent(original.findNeighborsOf(cell).size(), i -> new HashSet<>()).add(cell));
+		return new Universe(cellsByNeighbors.getOrDefault(2, Collections.emptySet()).toArray(new Cell[]{}));
 	}
 }
---
+++ src/test/java/ro/contezi/SurvivingCellsTest.java
@@ -0,0 +1,24 @@
+package ro.contezi;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.Collections;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SurvivingCellsTest {
+
+	private Evolver evolver;
+
+	@Before
+	public void setUp() {
+		evolver = new Evolver();
+	}
+
+	@Test
+	public void cellWithTwoNeighborsSurvives() throws Exception {
+		assertThat(evolver.evolve(new Universe(Cell.at(0, 0), Cell.at(1, 0), Cell.at(2, 0))).getCells())
+				.isEqualTo(Collections.singleton(Cell.at(1, 0)));
+	}
+}
